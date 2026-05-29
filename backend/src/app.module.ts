3ff0db804a33db698a53b7c2f66530f8016e99ad
Danay <danay@Notebook-2.local>
2026-05-29 16:01:57 +0300

    feat: auth module, user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service.js';
+import { AuthModule } from './auth/auth.module.js';
+import { UserModule } from './user/user.module.js';
 
 @Module({
-  imports: [],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
